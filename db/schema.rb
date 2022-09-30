0bc00882337601295a06fe184a47b503bea84255
NguyenDuong21 <thien123111999@gmail.com>
2022-09-30 10:22:22 +0700

    Add column4

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_29_081759) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_031909) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "test_column"
+    t.string "test4_column"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
 end
