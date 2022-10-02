0c1725bcb97d3f088ce9551e3f0b23f2a9b71ce4
nguyenduong21 <thien123111999@gmail.com>
2022-10-02 21:12:30 +0700

    Chuong10-Updating, showing, and

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ gem "bootstrap-sass", "3.4.1"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 gem "bcrypt", "3.1.13"
+gem "faker", "2.1.2"
+gem "will_paginate"
+gem "bootstrap-will_paginate"
+
 # Use mysql as the database for Active Record
 gem "mysql2", "~> 0.5"
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,6 @@ module SessionsHelper
     end
     
     def current_user
-        puts "logout"
         if (user_id = session[:user_id])
             @current_user ||= User.find_by(id: user_id)
         elsif (user_id = cookies.encrypted[:user_id])
@@ -21,17 +20,35 @@ module SessionsHelper
             end
         end    
     end
+
+    def current_user?(user)
+        user && user == current_user
+    end
+    
     def logged_in?
         !current_user.nil?
     end
+
     def forget(user)
         user.forget
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
     end
+    
     def log_out
         forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 end
