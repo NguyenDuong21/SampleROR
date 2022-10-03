72f92befbe973dba10d5d24b41c0709e3fb2caaf
NguyenDuong21 <thien123111999@gmail.com>
2022-10-03 10:19:50 +0700

    Chuong11-Account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,21 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
     # Returns the hash digest of the given string.
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-        BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
+
+    class << self
+    
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+            BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+        # Returns a random token.
+        def new_token
+            SecureRandom.urlsafe_base64
+        end
+
     end
-    # Returns a random token.
-    def User.new_token
-        SecureRandom.urlsafe_base64
-    end
+    
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
@@ -32,7 +38,6 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, nil)
     end
 
-
     # Activates an account.
     def activate
         update_columns(activated: true, activated_at: Time.zone.now)
@@ -40,6 +45,8 @@ class User < ApplicationRecord
     # Sends activation email.
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
+    end
+    
     private
 
     def downcase_email
@@ -52,17 +59,4 @@ class User < ApplicationRecord
         self.activation_digest = User.digest(activation_token)
     end
         
-
-    private
-
-    def downcase_email
-        self.email = email.downcase
-    end
-    
-    def create_activation_digest
-        # Create the token and digest.
-        self.activation_token = User.new_token
-        self.activation_digest = User.digest(activation_token)
-    end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,14 @@ User.create!(name: "Example User",
     activated_at: Time.zone.now)
     # Generate a bunch of additional users.
 99.times do |n|
-    name = "example-#{n+1}@"
+    name = Faker::Name.name
     email = "example-#{n+1}@railstutorial.org"
     password = "password"
     User.create!(name: name,
     email: email,
     password: password,
     password_confirmation: password,
-    activated: true,
-    activated_at: Time.zone.now)
+    activated: false,
+    activated_at: nil)
 end
     
