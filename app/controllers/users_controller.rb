847c0b40f2b2b4a74b1bf491aa03a917b493b690
NguyenDuong21 <thien123111999@gmail.com>
2022-10-04 13:33:36 +0700

    Merge branch main

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,28 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    unless @user
+      flash[:warning] = t(".mess_warning")
+      redirect_to root_url
+    end
   end
 
   def new
-    puts "new"
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = t("welcome")
       redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
