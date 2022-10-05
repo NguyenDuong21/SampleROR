c735b591c42df2f88e4227fffa08fa8dd542a9bc
NguyenDuong21 <thien123111999@gmail.com>
2022-10-05 11:16:18 +0700

    Chuong14-Following users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,11 @@ class StaticPagesController < ApplicationController
   
   def home
     @field = "This is home page"
-    @micropost = current_user.microposts.build if logged_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+    
   end
 
   def help
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
-    
+    has_many :active_relationships, class_name: "Relationship", 
+                                    foreign_key: "follower_id", 
+                                    dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent: :destroy
+
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
     attr_accessor :remember_token, :activation_token, :reset_token
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -10,10 +19,10 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-    
-    has_secure_password
-    # Returns the hash digest of the given string.
 
+    has_secure_password
+
+    # Returns the hash digest of the given string.
     class << self
     
         def digest(string)
@@ -32,6 +41,7 @@ class User < ApplicationRecord
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
     end
+    
     # Returns true if the given token matches the digest.
     def authenticated?(attribute, token)
         digest = send("#{attribute}_digest")
@@ -72,20 +82,36 @@ class User < ApplicationRecord
     # See "Following users" for the full implementation.
     def feed
       # Micropost.where("user_id = ?", id)
-      microposts
+      # following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+      # Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+
+      part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+      Micropost.joins(user: :followers).where(part_of_feed, { id: id })
     end
+
+    # Follows a user.
+    def follow(other_user)
+      following << other_user
+    end
+    # Unfollows a user.
+    def unfollow(other_user)
+      following.delete(other_user)
+    end
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
+    end
+  
 
     private
 
     def downcase_email
         self.email = email.downcase
     end
-    
+    # Creates and assigns the activation token and digest.
     def create_activation_digest
-
-        # Create the token and digest.
-        self.activation_token = User.new_token
-        self.activation_digest = User.digest(activation_token)
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
         
 end
